4ce3ea3fac9f2220c72f6789261810dea13491fd
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-03-02 01:01:38 +0800

    add

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -12,17 +12,21 @@ namespace Scripts
 		[SerializeField, Min(0f)] private float focusRadius = 1f;
 		[SerializeField, Range(0f, 1f)] private float focusCentering = 0.5f;
 		[SerializeField, Range(1f, 360f)] private float rotationSpeed = 90f;
+		[SerializeField, Range(-89f, 89f)] private float minVerticalAngle = -30f, maxVerticalAngle = 60f;
+		[SerializeField, Min(0f)] private float alignDelay = 5f;
+
 
 		private InputsManager inputManager;
 
-
-		private Vector3 focusPoint;
-		private Vector2 orbitAngles = new Vector2(45f, 0f);
+		private Vector3 focusPoint, previousFocusPoint;
+		private Vector2 orbitAngles = new Vector2(22.5f, 0f);
+		private float lastManualRotationTime;
 
 		private void Awake()
 		{
 			inputManager = InputsManager.Instance;
 			focusPoint = focus.position;
+			transform.localRotation = Quaternion.Euler(orbitAngles);
 		}
 
 		private void OnDestroy()
@@ -30,12 +34,30 @@ namespace Scripts
 			inputManager?.OnDestroy();
 		}
 
-		void LateUpdate()
+		private void OnValidate()
 		{
+			if (maxVerticalAngle < minVerticalAngle)
+			{
+				maxVerticalAngle = minVerticalAngle;
+			}
+		}
+
+		private void LateUpdate()
+		{
+			previousFocusPoint = focusPoint;
 			UpdateFocusPoint();
-			ManualRotation();
-			Quaternion lookRotation = Quaternion.Euler(orbitAngles);
-			Vector3 lookDirection = lookRotation * transform.forward;
+			Quaternion lookRotation;
+			if (ManualRotation() || AutomaticRotation())
+			{
+				ConstrainAngles();
+				lookRotation = Quaternion.Euler(orbitAngles);
+			}
+			else
+			{
+				lookRotation = transform.localRotation;
+			}
+
+			Vector3 lookDirection = lookRotation * Vector3.forward;
 			Vector3 lookPosition = focusPoint - lookDirection * distance;
 			transform.SetPositionAndRotation(lookPosition, lookRotation);
 		}
@@ -65,7 +87,7 @@ namespace Scripts
 			}
 		}
 
-		private void ManualRotation()
+		private bool ManualRotation()
 		{
 			Vector2 input = Vector2.zero;
 			float intensity = 0.0f;
@@ -85,13 +107,60 @@ namespace Scripts
 				intensity = 0.5f;
 			}
 
-			(input.x, input.y) = (-input.y * intensity, -input.x * intensity);
+			(input.x, input.y) = (-input.y * intensity, input.x * intensity);
 
-			const float e = 0.001f;
+			const float e = float.Epsilon;
 			if (input.x is < -e or > e || input.y is < -e or > e)
 			{
 				orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+				lastManualRotationTime = Time.unscaledTime;
+				return true;
 			}
+
+			return false;
+		}
+
+		private void ConstrainAngles()
+		{
+			orbitAngles.x = Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngle);
+
+			if (orbitAngles.y < 0.0f)
+			{
+				orbitAngles.y += 360f;
+			}
+			else if (orbitAngles.y >= 360f)
+			{
+				orbitAngles.y -= 360.0f;
+			}
+		}
+
+		private static float GetAngle(Vector2 direction)
+		{
+			float angle = Mathf.Acos(direction.y) * Mathf.Rad2Deg;
+			return direction.x < 0f ? 360f - angle : angle;
+		}
+
+		private bool AutomaticRotation()
+		{
+			if (Time.unscaledTime - lastManualRotationTime < alignDelay)
+			{
+				return false;
+			}
+
+			Vector2 movement = new Vector2(
+				focusPoint.x - previousFocusPoint.x,
+				focusPoint.z - previousFocusPoint.z
+			);
+			float movementDeltaSqr = movement.sqrMagnitude;
+			if (movementDeltaSqr < 0.0001f)
+			{
+				return false;
+			}
+
+			float headingAngle = GetAngle(movement / Mathf.Sqrt(movementDeltaSqr));
+			orbitAngles.y = headingAngle;
+
+			return true;
 		}
 	}
 }
