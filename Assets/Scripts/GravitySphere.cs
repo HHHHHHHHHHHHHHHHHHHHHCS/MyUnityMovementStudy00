19a2f5d2b3dd8abb9bf17bc798eec6dcdf4cc096
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2023-04-11 01:00:46 +0800

    add

---
+++ Assets/Scripts/GravitySphere.cs
@@ -0,0 +1,81 @@
+using System;
+using UnityEngine;
+
+namespace Scripts
+{
+	public class GravitySphere : GravitySource
+	{
+		[SerializeField] private float gravity = 9.81f;
+
+		[SerializeField] private float outerRadius = 10.0f;
+
+		[SerializeField] private float outerFalloffRadius = 15f;
+
+		[SerializeField, Min(0)] private float innerFalloffRadius = 1f, innerRadius = 5f;
+
+		private float innerFalloffFactor, outerFalloffFactor;
+
+		void Awake()
+		{
+			OnValidate();
+		}
+
+		void OnValidate()
+		{
+			innerFalloffRadius = Mathf.Max(innerRadius, 0f);
+			innerRadius = Mathf.Max(innerRadius, innerFalloffRadius);
+			outerRadius = Mathf.Max(outerRadius, innerRadius);
+			outerFalloffRadius = Mathf.Max(outerFalloffRadius, outerRadius);
+
+			innerFalloffFactor = 1f / (innerRadius - innerFalloffRadius);
+			outerFalloffFactor = 1f / (outerFalloffRadius - outerRadius);
+		}
+
+		public override Vector3 GetGravity(Vector3 position)
+		{
+			Vector3 vector = transform.position - position;
+			float distance = vector.magnitude;
+			if (distance > outerFalloffRadius || distance < innerFalloffRadius)
+			{
+				return Vector3.zero;
+			}
+
+			float g = gravity / distance;
+
+			if (distance > outerRadius)
+			{
+				g *= 1f - (distance - outerRadius) * outerFalloffFactor;
+			}
+			else if (distance < innerRadius)
+			{
+				g *= 1f - (innerRadius - distance) * innerFalloffFactor;
+			}
+
+			return g * vector;
+		}
+
+
+		private void OnDrawGizmos()
+		{
+			Vector3 p = transform.position;
+			if (innerFalloffRadius > 0f && innerFalloffRadius < innerRadius)
+			{
+				Gizmos.color = Color.cyan;
+				Gizmos.DrawWireSphere(p, innerFalloffRadius);
+			}
+
+			Gizmos.color = Color.yellow;
+			if (innerRadius > 0f && innerRadius < outerRadius)
+			{
+				Gizmos.DrawWireSphere(p, innerRadius);
+			}
+
+			Gizmos.DrawWireSphere(p, outerRadius);
+			if (outerFalloffRadius > outerRadius)
+			{
+				Gizmos.color = Color.cyan;
+				Gizmos.DrawWireSphere(p, outerFalloffRadius);
+			}
+		}
+	}
+}
